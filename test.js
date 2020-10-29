ec373de659e95116a001f86e210428b32cd58e12
Ludwig Schubert <ludwigschubi1@gmail.com>
2020-10-29 13:38:04 +0100

    Add option for custom namespaces

--- test.js
+++ test.js
@@ -1,13 +1,16 @@
-const assert = require('assert');
+const assert = require("assert");
 
 //Test the non-rdflib functionality
-const ns = require('./index.js')();
-assert.equal(ns.schema('Recipe'), 'http://schema.org/Recipe');
-
+const ns = require("./index.js")();
+assert.equal(ns.schema("Recipe"), "http://schema.org/Recipe");
 
 //Test the rdflib functionality
 const rdflib = {
-    namedNode: val => 'RDF::'+val
+  namedNode: (val) => "RDF::" + val,
 };
-const rdfns = require('./index.js')(rdflib);
-assert.equal(rdfns.schema('Recipe'), 'RDF::http://schema.org/Recipe');
+const rdfns = require("./index.js")(rdflib);
+assert.equal(rdfns.schema("Recipe"), "RDF::http://schema.org/Recipe");
+
+//Test custom namespace functionality
+const customns = require("./index.js")(rdflib, './testns.json');
+assert.equal(customns.example("Recipe"), "RDF::https://example.com/Recipe");
